3cd60496721da74691430245ba8c3abf3ab17e03
Mohammad Fadhil Nouhar A P <mohammad.fadhil@vonnue.com>
2026-07-27 17:43:48 +0530

    feat: Conditional and infer types

---
+++ Day-2/dist/infer.js
@@ -0,0 +1,17 @@
+let arr = [];
+let isArray;
+let arr1 = [[[["depth"]]]];
+let flatten;
+function func(arg) {
+    return arg;
+}
+let returns;
+function func1(text, id, arg3) {
+    return "test";
+}
+let args;
+function func2(...args) {
+    console.log(args);
+}
+func2("test", 1, { id: 1, name: "Max" });
+export {};
